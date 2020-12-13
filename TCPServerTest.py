7a9f381b81574a01d31da1fa0a278aee21140087
Abhiram Shibu <abhiramshibu1998@gmail.com>
2020-12-13 15:57:25 +0530

    TCPServer: Added recieve test code for TCP Server

---
+++ TCPServerTest.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 13 15:39:36 2020
+
+@author: Abhiram Shibu
+"""
+
+import TCPServer
+ts = TCPServer.tcpServer()
+ts.start()
+u = ts.acceptConnection()
+try:
+    while(True):
+        c=input("1) Check Available Data \n2) Read Available Data\n0) Exit\nenterchoice:")
+        if (int(c)==1):
+            print(u.available())
+        elif(int(c)==2):
+            print(u.read())
+        else:
+            break
+    u.stop()
+except KeyboardInterrupt:
+    u.stop()
+    print("Keyboard interrupt! Exiting")

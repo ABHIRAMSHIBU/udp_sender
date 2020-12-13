b3675a296d33b8c98c4cf085793812797936b2a8
Abhiram Shibu <abhiramshibu1998@gmail.com>
2020-12-13 15:50:59 +0530

    TCPServer: Added classes for TCP Server

---
+++ TCPServer.py
@@ -0,0 +1,50 @@
+class tcpServerHandler:
+    def __init__(self,connection,address):
+        self.data=b""
+        self.connection=connection
+        self.address=address
+    def recieve(self):
+        from threading import Thread
+        self.connection.settimeout(1)
+        def run():
+            import socket
+            self.run=True
+            while(self.run):
+                try:
+                    data = self.connection.recv(1024)
+                    self.data+=data
+                except socket.timeout:
+                    pass
+        self.t=Thread(target=run)
+        self.t.start()
+    def read(self):
+        data=self.data
+        self.data=b""
+        return data
+    def available(self):
+        return len(self.data)
+    def stop(self):
+        self.run=False
+    def __del__(self):
+        self.run=False
+class tcpServer:
+    def __init__(self,host="localhost",port=7011):
+        self.host=host
+        self.port=port
+        self.handlerList=[]
+    def start(self):
+        import socket
+        self.s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.bind((self.host, self.port))
+        self.s.listen()
+    def acceptConnection(self,*args):
+        connection, address = self.s.accept()
+        handler = tcpServerHandler(connection,address)
+        handler.recieve()
+        self.handlerList.append(handler)
+        return(handler)
+    def destroyConnection(self,handler):
+        if(type(handler)==type(1)):
+            handler=handlerList[handler]
+        handler.stop()
+        handlerList.pop(handlerList.index(handler))

d4adb44bb70916343f0172fdf069c5a0fa69b84f
Abhiram Shibu <abhiramshibu1998@gmail.com>
2020-12-13 14:48:48 +0530

    UDPServer: Added keyboard interrupt handler and thread stopper

--- UDPServerTest.py
+++ UDPServerTest.py
@@ -1,11 +1,16 @@
 import UDPServer
 u=UDPServer.udp()
 u.recieve()
-while(True):
-    c=input("1 available \n2 read data\n0 exit\nenterchoice:")
-    if (int(c)==1):
-        print(u.available())
-    elif(int(c)==2):
-        print(u.read())
-    else:
-        break
+try:
+    while(True):
+        c=input("1 available \n2 read data\n0 exit\nenterchoice:")
+        if (int(c)==1):
+            print(u.available())
+        elif(int(c)==2):
+            print(u.read())
+        else:
+            break
+    u.stop()
+except KeyboardInterrupt:
+    u.stop()
+    print("Keyboard interrupt! Exiting")

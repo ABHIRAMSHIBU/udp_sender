112fbb137ceeb7ff7914156d8bc10021bc277d60
Abhiram Shibu <abhiramshibu1998@gmail.com>
2020-12-27 12:14:21 +0530

    fileTree: Tree representation of file added
    Use getNode function to read a specific tree node
    
    Use setNode function to write a specific tree node
    
    Use getTotelNodes to find out how many nodes there are
    
    Co-authored-by: Jerome Joseph <jeromethanikkal@gmail.com>
    Co-authored-by: Raghunathan P <raghunathanp95@gmail.com>

---
+++ FileTree.py
@@ -0,0 +1,29 @@
+class fileTree:
+    def __int__(self, file, chunksize, read=True):
+        self.filename = file
+        if (read):
+            self.file = open(file, "rb")
+        else:
+            self.file = open(file, "wb")
+        self.chunksize = chunksize
+
+    def getNode(self, nodeid):
+        start = nodeid * self.chunksize
+        self.file.seek(start)
+        node = self.file.read(self.chunksize)
+        end = self.file.tell()
+        return node
+
+    def setNode(self, nodeid, node):
+        start = nodeid * self.chunksize
+        self.file.seek(start)
+        bytes = self.file.write(node)
+        end = self.file.tell()
+        return bytes
+
+    def getTotalNodes(self):
+        import math
+        f = open(self.filename, "ab")
+        size = f.tell()
+        f.close()
+        return math.ceil(size / self.chunksize)

a28dc7e8401886e3e85580097f92e87c283c652c
Preet Patel <preet.makz@gmail.com>
2021-01-12 21:54:05 +0530

    Added argument parser

--- ncTCPClientFileTest.py
+++ ncTCPClientFileTest.py
@@ -49,11 +49,61 @@ def packetCreate(data, name=None):
     packet += b"\n"
     return packet
 
+def argParse():
+    import sys
+    argv = sys.argv
+    args = {"tcpport": 5006, "udpport": 5005, "ip": "127.0.0.1", "file": "testfiles/test.png"}
+    for i in range(len(argv)):
+        if (i == 0):
+            continue
+        if ((argv[i] == "--tcpport") or (argv[i] == "-tp")):
+            try:
+                args["tcpport"] = int(argv[i + 1])
+            except IndexError:
+                print("Catastrophic Failure, please give tcp port")
+                exit(-1)
+        elif ((argv[i] == "--udpport") or (argv[i] == "-up")):
+            try:
+                args["udpport"] = int(argv[i + 1])
+            except IndexError:
+                print("Catastrophic Failure, please give udp port")
+                exit(-1)
+        elif ((argv[i] == "--host") or (argv[i] == "-h")):
+            try:
+                args["ip"] = int(argv[i + 1])
+            except IndexError:
+                print("Catastrophic Failure, please give udp port")
+                exit(-1)
+        elif ((argv[i] == "--file") or (argv[i] == "-f")):
+            try:
+                args["file"] = str(argv[i + 1])
+            except IndexError:
+                print("Catastrophic Failure, please give udp port")
+                exit(-1)
+        elif ((argv[i] == "--help") or (argv[i] == "-h")):
+            print('''UDP Sender Alpha V1.0
+    -tp or --tcpport is used to set tcp port
+    -up or --udpport is used to set udp port
+    -h  or --host    is used to set host ip
+    -f  or --file    is used to set which file to send
+Example
+    ''' + argv[0] + ''' -tp 5006 -up 5005 -h 192.168.1.2''')
+            exit(0)
+    return args
+
+
+args = argParse()
+print(args)
+
+#Determining file name
+fname = args["file"].split("/")[-1]
+
 
 ncTC = ncTCPClient()
 time.sleep(0.5)
 ncTC.action = packetCheck
-data=open("testfiles/test.png","rb").read()
-ncTC.write(packetCreate(data,"test.png"))
-print("Send file as test.png")
+#data=open("testfiles/test.png","rb").read()
+data=open(args["file"],"rb").read()
+ncTC.write(packetCreate(data,fname))
+print("Send file as ",fname)
 ncTC.join()
